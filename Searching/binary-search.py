fe425c648a39774927c5c9d5a5d61086f2d02dbc
andyonethousand <54973763+andyonethousand@users.noreply.github.com>
2020-04-05 18:43:39 -0700

    Update binary-search.py

--- Searching/binary-search.py
+++ Searching/binary-search.py
@@ -1,16 +1,20 @@
 def binary_search(list_of_nums, num):
+    list_of_nums = sorted(list_of_nums)
     first_index = 0
     last_index = len(list_of_nums) - 1
     found = False
-
+    
     while (first_index <= last_index and found != True):
+        # if you don't do //, then mid_index might not be an integer
         mid_index = (first_index + last_index) // 2
         if list_of_nums[mid_index] == num:
             found = True
         else:
             if num < list_of_nums[mid_index]:
+                # last_index can't be mid_index. because we already know list_of_nums[mid_index] isn't the num
                 last_index = mid_index - 1
             else:
+                # same thing
                 first_index = mid_index + 1
     return found
 
